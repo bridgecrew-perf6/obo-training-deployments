1195818c4ddb283a91e3a532458298bdd285d7cf
elmonazareno <elmonazareno@gmail.com>
2021-01-29 10:53:47 +0800

    added patch in emailing obo report

--- appserver/obo/server-modules/rameses-gov-etracs-obo-services.jar/sql/occupancy_permit_doc.sql
+++ appserver/obo/server-modules/rameses-gov-etracs-obo-services.jar/sql/occupancy_permit_doc.sql
@@ -15,10 +15,12 @@ AND dt.issuetype = 3
 [findDocsToReleaseCount]
 SELECT COUNT(*) AS count    
 FROM occupancy_permit_doc bs 
+INNER JOIN occupancy_permit oc ON bs.appid = oc.objid 
+INNER JOIN occupancy_permit_task ot ON oc.taskid = ot.taskid  
 INNER JOIN obo_doctype od ON bs.doctypeid = od.objid
 LEFT JOIN obo_section os ON od.sectionid=os.objid
 WHERE od.objid = $P{doctypeid} 
-AND bs.state = 1
+AND ot.state = 'releasing'
 AND bs.controlid IS NULL
 AND od.role IN ( ${roles} )  
 
